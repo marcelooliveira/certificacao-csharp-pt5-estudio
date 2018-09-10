50e54b93863a853c76c74962e91b821c88ee030c
marcelooliveira <mclricardo@gmail.com>
2018-09-10 18:05:09 -0300

    .

--- certificacao-csharp-pt5-estudio/Item 01/antes/01.1.TiposDeValor/Program.cs
+++ certificacao-csharp-pt5-estudio/Item 01/antes/01.1.TiposDeValor/Program.cs
@@ -13,21 +13,26 @@ namespace _01._1.TiposDeValor
         static void Metodo1()
         {
             Metodo2(12);
+            ///< image url="$(ProjectDir)\img1.png"/>
             Console.WriteLine("Saindo do método 1...");
         }
 
         static void Metodo2(int dados)
         {
             int multiplicador = 2;
-            Console.WriteLine("O valor é: " 
+            Console.WriteLine("O valor é: "
                 + dados.ToString());
             Metodo3(dados * multiplicador);
+            ///< image url="$(ProjectDir)\img2.png"/>
+
+            ///< image url="$(ProjectDir)\img4.png"/>
         }
 
         static void Metodo3(int dados)
         {
-            Console.WriteLine("O dobro é: " 
+            Console.WriteLine("O dobro é: "
                 + dados.ToString());
+            ///< image url="$(ProjectDir)\img3.png"/>
         }
     }
 }
