e7f69c82059305d79dbc60ae0d09e5baed6e9a1e
marcelooliveira <mclricardo@gmail.com>
2018-09-11 18:30:15 -0300

    .

--- certificacao-csharp-pt5-estudio/Item 01/antes/01.2.Coleta/Program.cs
+++ certificacao-csharp-pt5-estudio/Item 01/antes/01.2.Coleta/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
 
-namespace _01._02.Coleta
+namespace _01._02.TiposDeReferencia
 {
     class Program
     {
@@ -9,77 +9,46 @@ namespace _01._02.Coleta
         {
             await Task.Delay(3000); //aguarda 3 segundos
 
-            //await GerarTiposValor();
-
-            await GerarTiposReferencia();
-            //Quando são gerados muitos objetos,
-            //a imagem abaixo mostra que a quantidade de memória alocada
-            //aumenta bastante após 3 segundos, mas se mantém estável, pois
-            //nesse momento o coletor de lixo age para 
-            //liberar os objetos que deixaram de ser utilizados
-            //<image url="$(ProjectDir)\img2.png"/>
+            GerarTiposValor();
 
             Console.ReadKey();
         }
 
-        private static async Task GerarTiposValor()
+        private static void GerarTiposValor()
         {
-            for (long i = 0; i < 100; i++)
+            for (long i = 0; i < 100000; i++)
             {
-                byte nivelDeAzul = 0xFF;
-                int populacao = 1500;
-                short passageirosVoo = 230;
-                long populacaoDoBrasil = 207_660_929;
-
-                sbyte nivelDeBrilho = 127;
-                uint estoque = 1500;
-                ushort passageirosNavio = 230;
-                ulong populacaoDoMundo = 7_000_000_000;
-
-                bool fumante = false;
-                decimal salario = 5000m;
-                double massaDaTerra = 5.9736e24;
-                float numeroPI = 3.14159f;
-                await Task.Delay(3);
+                EstruturaLivro livro = new EstruturaLivro
+                {
+                    NumeroPaginas = 1200
+                };
             }
-
-            //a imagem abaixo mostra que a quantidade de memória alocada
-            //não varia significativamente
-            //<image url="$(ProjectDir)\img1.png"/>
         }
 
-        private static async Task GerarTiposReferencia()
+        private static void GerarTiposReferencia()
         {
-            for (long i = 0; i < 100; i++)
+            for (long i = 0; i < 100000; i++)
             {
-                Livro livro = new Livro();
-                await Task.Delay(3); //aguarda 3 milissegundos
+                ClasseLivro livro = new ClasseLivro
+                {
+                    Introducao = new string(' ', 100000),
+                    Texto = new string(' ', 100000),
+                    Conclusao = new string(' ', 100000)
+                };
             }
         }
     }
 
-    class Livro
+    struct EstruturaLivro
     {
-        string[] palavras = new string[1000000];
+        public double NumeroPaginas { get; set; }
+    }
 
-        ~Livro()
-        {
-            //    //O finalizador é invocado pelo coletor de lixo
-
-            //    //Aqui o livro deve ser finalizado
-            //    Console.WriteLine("Finalizador chamado...");
-
-            //    //Sabotando o coletor de lixo...
-            //    //Assim, o número de lixo sem ser coletado se acumula...
-
-            //    //CUIDADO AO HABILITAR O COMANDO SLEEP ABAIXO!
-            //    //Thread.Sleep(100);
-
-            //    //Note que agora a memória ocupada cresce absurdamente
-            //    //quando atrasamos a execução do coletor de lixo...
-            //    //<image url="$(ProjectDir)\img3.png"/>
-
-        }
+    class ClasseLivro
+    {
+        public string Introducao { get; set; }
+        public string Texto { get; set; }
+        public string Conclusao { get; set; }
     }
 }
 
