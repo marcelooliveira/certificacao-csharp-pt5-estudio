54eee9d883c3afe82aa80e33845cee332aabe514
marcelooliveira <mclricardo@gmail.com>
2018-09-20 16:08:10 -0300

    .

---
+++ certificacao-csharp-pt5-estudio/Item 02/antes/02.6 formatar/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _02._6_formatar
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var deLorean = new DeLorean();
+            Console.WriteLine(deLorean);
+        }
+    }
+
+    class DeLorean
+    {
+        public CapacitorDeFluxo Capacitor { get; } = new CapacitorDeFluxo();
+
+        public override string ToString()
+        {
+            return 
+                "DeLorean DMC-12\r\n"
+                + "===============\r\n"
+                + "\r\n"
+                + Capacitor;
+        }
+    }
+
+    class CapacitorDeFluxo
+    {
+        public const double VELOCIDADE_MPH = 88; //milhas por hora
+        public const double KMH_PARA_MPH = 1.60934; //fator de conversão para KMH
+
+        public override string ToString()
+        {
+            return 
+                "Capacitor de fluxo\r\n"
+                + "=================\r\n"
+                + "Velocidade mínima:\r\n"
+                + "\tEm milhas por hora:       MPH\r\n"
+                + "\tEm quilômetros por hora:  KMH";
+        }
+    }
+}
---
+++ certificacao-csharp-pt5-estudio/Item 02/depois/02.6 formatar/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _02._6_formatar
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var deLorean = new DeLorean();
+            Console.WriteLine(deLorean);
+        }
+    }
+
+    class DeLorean
+    {
+        public CapacitorDeFluxo Capacitor { get; } = new CapacitorDeFluxo();
+
+        public override string ToString()
+        {
+            return string.Format(
+                "DeLorean DMC-12\r\n"
+                + "===============\r\n"
+                + "\r\n{0}", Capacitor);
+        }
+    }
+
+    class CapacitorDeFluxo
+    {
+        public const double VELOCIDADE_MPH = 88; //milhas por hora
+        public const double KMH_PARA_MPH = 1.60934; //fator de conversão para KMH
+
+        public override string ToString()
+        {
+            return string.Format(
+                "Capacitor de fluxo\r\n"
+                + "=================\r\n"
+                + "Velocidade mínima:\r\n"
+                + "\tEm milhas por hora:      {0,10:N2} MPH\r\n"
+                + "\tEm quilômetros por hora: {1,10:N2} KMH"
+                , VELOCIDADE_MPH, VELOCIDADE_MPH * KMH_PARA_MPH);
+        }
+    }
+}
