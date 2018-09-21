257952785e849052137cf7de09068c976a0726c0
marcelooliveira <mclricardo@gmail.com>
2018-09-21 14:56:42 -0300

    .

--- certificacao-csharp-pt5-estudio/Item 02/antes/02.6 formatar/Program.cs
+++ certificacao-csharp-pt5-estudio/Item 02/antes/02.6 formatar/Program.cs
@@ -1,43 +1,75 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace _02._6_formatar
 {
-    public class Program
+    class Program
     {
-        public static void Main()
+        static void Main(string[] args)
         {
-            var deLorean = new DeLorean();
-            Console.WriteLine(deLorean);
-        }
-    }
+            var contrato = new
+            {
+                Empresa = "Alura Serviços Hidráulicos Ltda.",
+                Funcionario = "Mario Mario",
+                Inicio = new DateTime(2019, 1, 1),
+                Cargo = "encanador",
+                Salario = 3108.45,
+                InicioJornada = new DateTime(2018, 1, 10, 9, 0, 0),
+                FimJornada = new DateTime(2018, 1, 10, 18, 0, 0)
+            };
 
-    class DeLorean
-    {
-        public CapacitorDeFluxo Capacitor { get; } = new CapacitorDeFluxo();
+            string documento = 
+                string.Format(
+                @"                                             CONTRATO INDIVIDUAL DE TRABALHO TEMPORÁRIO
 
-        public override string ToString()
-        {
-            return 
-                "DeLorean DMC-12\r\n"
-                + "===============\r\n"
-                + "\r\n"
-                + Capacitor;
-        }
-    }
 
-    class CapacitorDeFluxo
-    {
-        public const double VELOCIDADE_MPH = 88; //milhas por hora
-        public const double KMH_PARA_MPH = 1.60934; //fator de conversão para KMH
+            EMPREGADOR: " + contrato.Empresa + @"
 
-        public override string ToString()
-        {
-            return 
-                "Capacitor de fluxo\r\n"
-                + "=================\r\n"
-                + "Velocidade mínima:\r\n"
-                + "\tEm milhas por hora:       MPH\r\n"
-                + "\tEm quilômetros por hora:  KMH";
+            EMPREGADO: " + contrato.Funcionario + @"
+
+
+Pelo presente instrumento particular de contrato individual de trabalho, fica justo e contratado o seguinte:
+ 
+Cláusula 1ª - O EMPREGADO prestará ao EMPREGADOR, a partir de " + contrato.Inicio + @" e assinatura deste instrumento, seus trabalhos exercendo a função de " + contrato.Cargo + @", prestando pessoalmente o labor diário no período compreendido entre " + contrato.InicioJornada + @" e " + contrato.FimJornada + @", e intervalo de 1 hora para almoço;
+
+            Cláusula 2ª - Não haverá expediente nos dias de sábado, sendo prestado a compensação de horário semanal;
+
+            Cláusula 3ª - O EMPREGADOR pagará mensalmente, ao EMPREGADO, a título de salário a importância de " + contrato.Salario + @", com os descontos previstos por lei;
+
+            Cláusula 4ª - Estará o EMPREGADO subordinado a legislação vigente no que diz respeito aos descontos de faltas e demais sanções disciplinares contidas na Consolidação das Leis do Trabalho.
+
+            Cláusula 5ª - O prazo de duração do contrato é de 2(dois) anos, contados a partir da assinatura pelos contratantes;
+
+            Cláusula 6ª - O EMPREGADO obedecerá o regulamento interno da empresa, e filosofia de trabalho da mesma.
+
+
+Como prova do acordado, assinam instrumento, afirmado e respeitando seu teor por inteiro, e firmam conjuntamente a este duas testemunhas, comprovando as razões descritas.
+
+
+São Paulo, " + DateTime.Today + @"
+
+
+
+_______________________________________________________
+" + contrato.Empresa + @"
+
+
+_______________________________________________________
+" + contrato.Funcionario + @"
+
+
+_______________________________________________________
+(Nome, R.G, Testemunha)
+
+
+_______________________________________________________
+(Nome, R.G, Testemunha)
+ ");
+            Console.WriteLine(documento);
+            Console.ReadKey();
         }
     }
 }
