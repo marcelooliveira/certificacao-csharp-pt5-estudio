c15d4b27a94201329d7e24e513eac3ca0e87bff1
marcelooliveira <mclricardo@gmail.com>
2018-09-21 19:09:39 -0300

    997-CertificacaoCSharpParte5-video2.9-Formando números e datas.trec

--- certificacao-csharp-pt5-estudio/Item 02/depois/02.6 formatar/Program.cs
+++ certificacao-csharp-pt5-estudio/Item 02/depois/02.6 formatar/Program.cs
@@ -8,6 +8,32 @@ namespace _02._6_formatar
 {
     class Program
     {
+        private const string MODELO = @"CONTRATO INDIVIDUAL DE TRABALHO TEMPORÁRIO
+            EMPREGADOR: {0} 
+            EMPREGADO: {1}
+Pelo presente instrumento particular de contrato individual de trabalho,
+fica justo e contratado o seguinte:
+ Cláusula 1ª - O EMPREGADO prestará ao EMPREGADOR, a partir de 
+{2:d} e assinatura deste instrumento, seus trabalhos 
+exercendo a função de {3}, prestando pessoalmente o 
+labor diário no período compreendido entre {4:t} e {5:t}, 
+e intervalo de 1 hora para almoço;
+            Cláusula 2ª - Não haverá expediente nos dias de sábado, sendo 
+prestado a compensação de horário semanal;
+            Cláusula 3ª - O EMPREGADOR pagará mensalmente, ao EMPREGADO, a 
+título de salário a importância de {6:C3}, com os 
+descontos previstos por lei;
+
+São Paulo, {7:D}
+_______________________________________________________
+{0}
+______________________________________________________
+{1}
+_______________________________________________________
+(Nome, R.G, Testemunha)
+_______________________________________________________
+(Nome, R.G, Testemunha)";
+
         static void Main(string[] args)
         {
             var contrato = new
@@ -22,39 +48,15 @@ namespace _02._6_formatar
             };
 
             string documento = string.Format(
-                @"CONTRATO INDIVIDUAL DE TRABALHO TEMPORÁRIO
-            EMPREGADOR: {0} 
-            EMPREGADO: {1}
-Pelo presente instrumento particular de contrato individual de trabalho,
-fica justo e contratado o seguinte:
- Cláusula 1ª - O EMPREGADO prestará ao EMPREGADOR, a partir de 
-{2} e assinatura deste instrumento, seus trabalhos 
-exercendo a função de {3}, prestando pessoalmente o 
-labor diário no período compreendido entre {4} e {5}, 
-e intervalo de 1 hora para almoço;
-            Cláusula 2ª - Não haverá expediente nos dias de sábado, sendo 
-prestado a compensação de horário semanal;
-            Cláusula 3ª - O EMPREGADOR pagará mensalmente, ao EMPREGADO, a 
-título de salário a importância de {6}, com os 
-descontos previstos por lei;
-
-São Paulo, {7}
-_______________________________________________________
-{0}
-______________________________________________________
-{1}
-_______________________________________________________
-(Nome, R.G, Testemunha)
-_______________________________________________________
-(Nome, R.G, Testemunha)"
-, contrato.Empresa
-, contrato.Funcionario
-, contrato.Inicio
-, contrato.Cargo
-, contrato.InicioJornada
-, contrato.FimJornada
-, contrato.Salario
-, DateTime.Today);
+                MODELO
+                , contrato.Empresa
+                , contrato.Funcionario
+                , contrato.Inicio
+                , contrato.Cargo
+                , contrato.InicioJornada
+                , contrato.FimJornada
+                , contrato.Salario
+                , DateTime.Today);
 
             Console.WriteLine(documento);
             Console.ReadKey();
